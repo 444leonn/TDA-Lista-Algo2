d945dd407a1085c76ea4e5e484f321c455f6f555
444leonn <acostaleon25@gmail.com>
2025-09-19 12:17:22 -0300

    feat: agregada funciones lista crear, vacia, cantidad, agregar y destructor

--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -4,6 +4,8 @@
 #include "src/pila.h"
 #include <stdlib.h>
 
+#define DATO_EJEMPLO 5
+
 void lista_crear_devuelve_la_lista()
 {
 	lista_t* lista = lista_crear();
@@ -25,14 +27,15 @@ void lista_vacia_devuelve_true_si_no_tiene_elementos()
 void lista_vacia_devuelve_false_si_tiene_elementos()
 {
 	lista_t* lista = lista_crear();
-	int* dato;
-	*dato = 5;
+	int* dato = malloc(sizeof(int));
+	*dato = DATO_EJEMPLO;
 	if (lista != NULL) {
 		lista_agregar(lista, dato);
 	}
 	pa2m_afirmar(lista_vacia(lista) == false, "Funcion lista_vacia devuelve false para una lista no vacia.");
 	if (lista != NULL) {
 		lista_destruir(lista);
+		free(dato);
 	}
 }
 
@@ -46,18 +49,18 @@ void lista_cantidad_devuelve_0_para_lista_nula()
 void lista_cantidad_devuelve_la_cantidad_de_elementos()
 {
 	lista_t* lista = lista_crear();
-	int* dato;
-	*dato = 5;
+	int* dato = malloc(sizeof(int));
+	*dato = DATO_EJEMPLO;
 	if (lista != NULL) {
 		lista_agregar(lista, dato);
 	}
-	lista_agregar(lista, dato);
 
 	size_t cantidad = lista_cantidad(lista);
 	pa2m_afirmar(cantidad == 1, "Lista cantidad devuelve la cantidad de elementos. (devolvio: %ld)", cantidad);
 
 	if (lista != NULL) {
 		lista_destruir(lista);
+		free(dato);
 	}
 }
 
@@ -72,15 +75,74 @@ void lista_agregar_devuelve_false_si_no_agrega()
 void lista_agregar_devuelve_true_si_agrega()
 {
 	lista_t* lista = lista_crear();
-	int* dato;
-	*dato = 5;
+	int* dato = malloc(sizeof(int));
+	*dato = DATO_EJEMPLO;
 
 	bool agregado = lista_agregar(lista, dato);
 	pa2m_afirmar(agregado == true, "Agregar un elemento la lista lo agrega.");
 
 	if (lista != NULL) {
 		lista_destruir(lista);
+		free(dato);
 	}
+}
+
+void lista_insertar_devuelve_null_para_posicion_invalida()
+{
+	//lista_t* lista = lista_crear();
+	//int* dato = malloc(sizeof(int));
+	//*dato = DATO_EJEMPLO;
+
+	//bool insertado = lista_insertar(lista, dato, 4);
+	//pa2m_afirmar(insertado == false, "Insertar un elemento en una posicion invalida no lo inserta, devuelve false.");
+	//if (lista != NULL) {
+	//	lista_destruir(lista);
+	//	free(dato);
+	//}
+}
+
+void lista_insertar_devuelve_true_para_posicion_valida()
+{
+	//lista_t* lista = lista_crear();
+	//int* dato = malloc(sizeof(int));
+	//*dato = DATO_EJEMPLO;
+
+	//bool insertado = lista_insertar(lista, dato, 4);
+	//pa2m_afirmar(insertado == true, "Insertar un elemento en una posicion valida lo inserta, devuelve true.");
+	//if (lista != NULL) {
+	//	lista_destruir(lista);
+	//	free(dato);
+	//}
+}
+
+void lista_eliminar_elemento_lista_vacia_devuelve_null()
+{
+	//lista_t* lista = lista_crear();
+	//int* eliminado = lista_eliminar_elemento(lista, 0);
+	//pa2m_afirmar(eliminado == NULL, "Eliminar un elemento de la lista inexistente retorna NULL.");
+	//if (lista != NULL) {
+	//	lista_destruir(lista);
+	//}
+}
+
+void lista_eliminar_elemento_devuelve_el_elemento()
+{
+	//lista_t* lista = lista_crear();
+	//if (lista != NULL) {
+		//int* dato = malloc(sizeof(int));
+		//*dato = DATO_EJEMPLO;
+		//lista_agregar(lista, dato);
+		//free(dato);
+	//}
+	//int* dato_eliminado = lista_eliminar_elemento(lista, 0);
+	//pa2m_afirmar(*dato_eliminado == DATO_EJEMPLO, "Eliminar un elemento de la lista retorna el elemento. (devolvio: %ld)", dato_eliminado);
+	//if (lista != NULL) {
+	//	lista_destruir(lista);
+//	}
+}
+
+void lista_buscar_posicion_no_encuentra_el_elemento()
+{
 }
 
 int main()
@@ -97,6 +159,13 @@ int main()
 	pa2m_nuevo_grupo("Pruebas de Agregar Elementos en Lista:");
 	lista_agregar_devuelve_false_si_no_agrega();
 	lista_agregar_devuelve_true_si_agrega();
+	//pa2m_nuevo_grupo("Pruebas de Inserscion Elementos en Lista:");
+	//lista_insertar_devuelve_null_para_posicion_invalida();
+	//lista_insertar_devuelve_true_para_posicion_valida();
+	//pa2m_nuevo_grupo("Pruebas de Eliminar Elementos en Lista:");
+	//lista_eliminar_elemento_lista_vacia_devuelve_null();
+//	lista_eliminar_elemento_devuelve_el_elemento();
+//	pa2m_nuevo_grupo("Pruebas de Busqueda Elemento en Lista:");
 
 
 	pa2m_nuevo_grupo("============== Pruebas de Pila ===============");
---
+++ src/lista.c
@@ -0,0 +1,72 @@
+#include "lista.h"
+
+struct lista {
+    void* dato;
+    lista_t* nodo_siguiente;
+};
+
+lista_t* lista_crear()
+{
+    return calloc(1, sizeof(lista_t));
+}
+
+bool lista_vacia(lista_t *lista)
+{
+    if (lista == NULL || lista->nodo_siguiente == NULL)
+        return true;
+    return false;
+}
+
+size_t lista_cantidad(lista_t *lista)
+{
+    if (lista == NULL)
+        return 0;
+
+    lista_t* puntero_inicio = lista;
+
+    size_t cantidad = 0;
+    while (lista->nodo_siguiente != NULL) {
+        lista = lista->nodo_siguiente;
+        cantidad++;
+    }
+
+    lista = puntero_inicio;
+
+    return cantidad;
+}
+
+bool lista_agregar(lista_t *lista, void *dato)
+{
+    if (lista == NULL)
+        return false;
+
+    lista_t* puntero_inicio = lista;
+    while (lista->nodo_siguiente != NULL) {
+        lista = lista->nodo_siguiente;
+    }
+
+    if (lista->nodo_siguiente == NULL) {
+        lista_t* nuevo_nodo = lista_crear();
+        nuevo_nodo->dato = dato;
+
+        lista->nodo_siguiente = nuevo_nodo;
+        lista = puntero_inicio;
+        return true;
+    }
+
+    return false;
+}
+
+void lista_destruir(lista_t *lista)
+{
+    if (lista == NULL)
+        return;
+
+    if (lista->nodo_siguiente == NULL) {
+        free(lista);
+        return;
+    }
+
+    lista_destruir(lista->nodo_siguiente);
+    free(lista);
+}
