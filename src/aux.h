4d60194b0d804a9dd8736774ef5bc49837c8a5de
444leonn <acostaleon25@gmail.com>
2025-09-20 19:31:33 -0300

    feat: modif la implementacion, sera por nodos

--- src/aux.h
+++ src/aux.h
@@ -1,6 +1,11 @@
 #ifndef AUX_H_
 #define AUX_H_
 
+typedef nodo {
+    void* dato;
+    nodo_t* nodo_siguiente;
+} nodo_t;
+
 int comparador_enteros(const void* primer_dato, const void* segundo_dato);
 
 void destructor_enteros(void* dato_entero);
