b1c38fb8a6e2dafe87ad118918724a4e4f04b5a6
444leonn <acostaleon25@gmail.com>
2025-09-20 19:44:22 -0300

    feat: modificadas las funciones para cumplir con la nueva implementacion

--- src/lista.c
+++ src/lista.c
@@ -6,99 +6,30 @@ struct lista {
     size_t cantidad;
 };
 
-lista_t* lista_crear()
+lista_t *lista_crear()
 {
     return calloc(1, sizeof(lista_t));
 }
 
 bool lista_vacia(lista_t *lista)
 {
-    if (lista == NULL || (lista->nodo_siguiente == NULL && lista->dato == NULL))
-        return true;
-    return false;
+    if (lista == NULL)
+        return false;
+    return lista->cantidad == 0;
 }
 
 size_t lista_cantidad(lista_t *lista)
 {
     if (lista == NULL)
         return 0;
-
-    lista_t* puntero_inicio = lista;
-
-    size_t cantidad = 0;
-    while (lista->nodo_siguiente != NULL) {
-        lista = lista->nodo_siguiente;
-        cantidad++;
-    }
-
-    lista = puntero_inicio;
-
-    return cantidad;
+    return lista->cantidad;
 }
 
 bool lista_agregar(lista_t *lista, void *dato)
 {
     if (lista == NULL || dato == NULL)
         return false;
+    
+    
 
-    if (lista->nodo_siguiente == NULL) {
-        lista->dato = dato;
-        lista->nodo_siguiente = lista_crear();
-        return true;
-    }
-
-    return lista_agregar(lista->nodo_siguiente, dato);
-}
-
-int lista_buscar_posicion(lista_t* lista, void* elemento, int (*comparador)(const void*, const void*))
-{
-    int posicion = -1, i = 0;
-
-    if (lista == NULL || elemento == NULL || comparador == NULL || lista_vacia(lista))
-        return posicion;
-
-    lista_t *p_inicio_lista = lista;
-    bool encontrado = false;
-
-
-    while (lista->nodo_siguiente != NULL && !encontrado) {
-        if (comparador(lista->dato, elemento) == 0) {
-            encontrado = true;
-            posicion = i;
-        }
-        lista = lista->nodo_siguiente;
-        i++;
-    }
-
-    lista = p_inicio_lista;
-
-    return posicion;
-}
-
-void lista_destruir_todo(lista_t* lista, void (*destructor)(void*))
-{
-    if (lista == NULL || destructor == NULL)
-        return;
-
-    if (lista->nodo_siguiente == NULL) {
-        destructor(lista->dato);
-        free(lista);
-        return;
-    }
-    lista_destruir_todo(lista->nodo_siguiente, destructor);
-    destructor(lista->dato);
-    free(lista);
-}
-
-void lista_destruir(lista_t *lista)
-{
-    if (lista == NULL)
-        return;
-
-    if (lista->nodo_siguiente == NULL) {
-        free(lista);
-        return;
-    }
-    lista_destruir(lista->nodo_siguiente);
-    free(lista);
 }
