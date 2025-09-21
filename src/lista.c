4243f6784cfee74dc21464d60112d04693d90c46
444leonn <acostaleon25@gmail.com>
2025-09-21 14:52:43 -0300

    feat: implementado el agregar recursivo

--- src/lista.c
+++ src/lista.c
@@ -24,31 +24,25 @@ size_t lista_cantidad(lista_t *lista) {
     return lista->cantidad;
 }
 
-nodo_t *agregar_recursivo(nodo_t *nodo, void *dato) {
-    if (nodo == NULL) {
-      nodo_t *nuevo_nodo = malloc(sizeof(nodo_t));
-      if (nuevo_nodo == NULL)
-        return NULL;
-      nuevo_nodo->dato = dato;
-      nuevo_nodo->nodo_siguiente = NULL;
-      return nuevo_nodo;
+bool lista_agregar(lista_t *lista, void *dato)
+{
+    if (lista == NULL)
+        return false;
+    
+    nodo_t* p_nodo = lista->primer_nodo;
+    while (p_nodo->nodo_siguiente != NULL) {
+        p_nodo = p_nodo->nodo_siguiente;
+    }
+    
+    if (p_nodo->nodo_siguiente == NULL) {
+        p_nodo->nodo_siguiente = calloc(1, sizeof(nodo_t));
+        if (p_nodo->nodo_siguiente == NULL)
+            return false;
+        p_nodo->dato = dato;
+        lista->cantidad++;
     }
 
-    nodo->nodo_siguiente = agregar_recursivo(nodo->nodo_siguiente, dato);
-    return nodo;
-}
-
-bool lista_agregar(lista_t *lista, void *dato) {
-    if (lista == NULL || dato == NULL)
-      return false;
-
-    lista->primer_nodo = agregar_recursivo(lista->primer_nodo, dato);
-    if (lista->primer_nodo != NULL) {
-      lista->cantidad++;
-      return true;
-    }
-
-    return false;
+    return true;
 }
 
 nodo_t *insertar_recursivo(nodo_t *nodo, void *dato, size_t posicion) {
