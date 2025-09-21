9513490b60938b110140fc9d509722f72ad8deea
444leonn <acostaleon25@gmail.com>
2025-09-20 21:01:11 -0300

    feat: implementada funcion agregar

--- src/lista.c
+++ src/lista.c
@@ -1,4 +1,5 @@
 #include "lista.h"
+#include "aux.h"
 #include <stdio.h>
 
 struct lista {
@@ -25,11 +26,31 @@ size_t lista_cantidad(lista_t *lista)
     return lista->cantidad;
 }
 
+nodo_t* agregar_recursivo(noto_t* nodo, void* dato)
+{
+    if (nodo == NULL) {
+        nodo_t* nuevo_nodo = malloc(sizeof(nodo_t));
+        if (nuevo_nodo == NULL)
+            return NULL;
+        nuevo_nodo->dato = dato;
+        nuevo_nodo->nodo_siguiente = NULL;
+        return nuevo_nodo;
+    }
+
+    nodo->nodo_siguiente = agregar_recursivo(nodo_siguiente, dato);
+    return nodo;
+}
+
 bool lista_agregar(lista_t *lista, void *dato)
 {
     if (lista == NULL || dato == NULL)
         return false;
     
-    
+    lista->primer_nodo = agregar_recursivo(lista->primer_nodo, dato);
+    if (lista->primer_nodo != NULL) {
+        lista->cantidad++;
+        return true;
+    }
 
+    return false;
 }
