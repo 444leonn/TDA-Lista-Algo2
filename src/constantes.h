57e2047004c2b8c407c237c0c56c3ad213967738
444leonn <acostaleon25@gmail.com>
2025-09-23 02:05:53 -0300

    modif

--- src/constantes.h
+++ src/constantes.h
@@ -1,14 +1,14 @@
 #ifndef CONSTANTES_H_
 #define CONSTANTES_H_
 
-#define MODO_LECTURA "r"
-#define MODO_ESCRITURA "w"
-#define TAMAÑO_INICIAL 15
-#define CAPACIDAD_INICIAL 4
 #define TROJO "\x1b[31m"
 #define TNORMAL "\x1b[0m"
 #define TIPO_INVALIDO -1
 
+#define MODO_LECTURA "r"
+#define MODO_ESCRITURA "w"
+#define TAMAÑO_INICIAL 15
+#define CAPACIDAD_INICIAL 4
 #define DATO_EJEMPLO_1 5
 #define DATO_EJEMPLO_2 6
 #define DATO_EJEMPLO_3 7
