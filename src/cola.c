be9b55ec7835abd88a30b780412487c9062a6d59
444leonn <acostaleon25@gmail.com>
2025-09-23 01:32:32 -0300

    feat: definida estructura de cola

---
+++ src/cola.c
@@ -0,0 +1,19 @@
+#include "cola.h"
+#include "aux.h"
+
+struct cola {
+    nodo_t* nodo_cabecera;
+    size_t cantidad;
+};
+
+cola_t *cola_crear();
+
+bool cola_encolar(cola_t *cola, void *elemento);
+
+void *cola_desencolar(cola_t *cola);
+
+void *cola_ver_primero(cola_t *cola);
+
+size_t cola_cantidad(cola_t *cola);
+
+void cola_destruir(cola_t *cola);
